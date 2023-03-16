cde26d7cc86c27747ce7b1dd177f2f8aef84d0f3
HichamBC <yours.hichambc@gmail.com>
2023-03-16 21:06:10 +0000

    5-printf.c task

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: C program that prints exactly the line of text you want.
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

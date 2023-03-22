6406f1d621d38f1e6a7014cdf58361c99568d283
HichamBC <yours.hichambc@gmail.com>
2023-03-21 17:24:45 -0700

    10-add.c task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - Entry Point
+ *
+ * Description: function that adds two integers and returns the result.
+ *
+ * @i: first parameter
+ * @j: second parameter
+ *
+ * Return: sum of i and j.
+ */
+
+int add(int i, int j)
+{
+	int res;
+
+	res = i + j;
+	return (res);
+}

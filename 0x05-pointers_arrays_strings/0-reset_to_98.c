9852a217b8ccc8a440a8105cee60dd836a8c8086
HichamBC <yours.hichambc@gmail.com>
2023-03-27 18:21:10 +0000

    0-reset_to_98.c task

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - set the value that the pointer points to to 98.
+ *
+ * @n: a pointer to the integer we want to set to 98.
+ *
+ * Return: None.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+this an ALX project where i will be practicing and solving arrays and pointers related task.

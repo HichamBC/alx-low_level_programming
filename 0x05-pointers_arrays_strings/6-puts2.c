429b068f57ffe86a0c2eea5a62a1b0e57ab3cd25
HichamBC <yours.hichambc@gmail.com>
2023-03-28 00:03:45 +0000

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,12 +10,12 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
-		i += 2;
+		i++;
 	}
 	_putchar('\n');
 }

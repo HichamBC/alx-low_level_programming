58323c51dc05765a426c2d4a853a55948b29067c
HichamBC <yours.hichambc@gmail.com>
2023-03-31 01:47:33 +0000

    7-leet.c task

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337.
+ *
+ * @str: function pointer.
+ *
+ * Return: pointer to the resulting string.
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char *letters = "aAeEoOtTlL";
+	char *nums = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == letters[j])
+			{
+				str[i] = nums[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}

aa9c4844fd6c73cc16996384d80402e4a0248ab0
HichamBC <yours.hichambc@gmail.com>
2023-04-06 23:47:51 +0000

    5-sqrt_recursion.c task

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 void _print_rev_recursion(char *s);
+int _sqrt_recursion(int n);
+int guess_sqrt(int n, int guess);
 
 #endif

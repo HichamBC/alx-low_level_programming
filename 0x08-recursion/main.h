f6fb702bf9d168c7d531ba781e42f1cb613e1f07
HichamBC <yours.hichambc@gmail.com>
2023-04-07 01:10:41 +0000

    6-is_prime_number.c task

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,7 @@ int _pow_recursion(int x, int y);
 void _print_rev_recursion(char *s);
 int _sqrt_recursion(int n);
 int guess_sqrt(int n, int guess);
+int is_prime_number(int n);
+int is_divisible(int n, int factor);
 
 #endif

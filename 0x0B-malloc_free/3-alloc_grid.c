d204d442d171bb8204a99e45b188641cc1a4a8cf
HichamBC <yours.hichambc@gmail.com>
2023-04-12 00:16:54 +0000

    3-alloc_grid.c task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,13 +27,13 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 		matrix[i] = malloc(columns);
 
-	if (matrix != NULL)
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
 	{
-		for (i = 0; i < height; i++)
-		{
-			for (j = 0; j < width; j++)
-				matrix[i][j] = 0;
-		}
+		for (j = 0; j < width; j++)
+			matrix[i][j] = 0;
 	}
 	return (matrix);
 }

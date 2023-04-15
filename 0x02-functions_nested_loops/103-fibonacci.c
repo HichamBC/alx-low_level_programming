600f7f66c33cacd546b2f3ac2e518cd3b6cf43ee
HichamBC <yours.hichambc@gmail.com>
2023-04-15 15:27:36 +0000

    103-fibonacci.c tasl

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point.
+ *
+ * Description: finds and prints the sum of the even-valued terms.
+ *
+ * Return: Always 0 (Success).
+ */
+
+int main(void)
+{
+	int i;
+	long int a = 1, b = 2, c, sum = 2;
+
+	while (c <= 4000000)
+	{
+		c = a + b;
+		a = b;
+		b = c;
+		if (c % 2 == 0)
+		{
+			sum += c;
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}

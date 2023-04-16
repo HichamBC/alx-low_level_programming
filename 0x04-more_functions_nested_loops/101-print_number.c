43d4aac2f8bad048c8449af25ffaca34fc04e68b
HichamBC <yours.hichambc@gmail.com>
2023-04-16 23:40:29 +0000

    101-print_number.c task

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer.
+ *
+ * @n: an integer.
+ *
+ * Return: None.
+ */
+
+void print_number(int n)
+{
+	int divisor = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	while (n / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+
+	while (divisor != 0)
+	{
+		_putchar(n / divisor + '0');
+		n %= divisor;
+		divisor /= 10;
+	}
+}

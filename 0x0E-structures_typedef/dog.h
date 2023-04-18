9fe62a439d5e253e0d7f065e7943c12791293042
HichamBC <yours.hichambc@gmail.com>
2023-04-18 02:12:04 +0000

    2-print_dog.c task

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
+
 /**
  * struct dog - structure data type.
  * @name: his name.
@@ -19,6 +20,6 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
 
 #endif

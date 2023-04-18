33adf10a10976ac9d3ef3b190dd829b4adcc9496
HichamBC <yours.hichambc@gmail.com>
2023-04-18 05:59:04 +0000

    4-new_dog.c task

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -45,8 +45,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		ownercp[j] = owner[j];
 	ownercp[j] = '\0';
 
-	d->name = name;
+	d->name = namecp;
+		free(d);
 	d->age = age;
-	d->owner = owner;
+	d->owner = ownercp;
 	return (d);
 }

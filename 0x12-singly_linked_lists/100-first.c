67a99cd4fc7a9fb983a835c88d2a126bcacb8d94
HichamBC <yours.hichambc@gmail.com>
2023-05-02 22:46:37 +0000

    100-first.c task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * first - prints a specified string before the main function.
+ *
+ * Return: None.
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

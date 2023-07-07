169cd2adcce6dd0c44181b0d968cbfaefe6e2a48
HichamBC <yours.hichambc@gmail.com>
2023-07-07 22:53:26 +0000

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,19 +13,23 @@
 int main(int argc, char *argv[])
 {
 	int i, sum = 0;
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			int num = atoi(argv[i]);
 
-			if (num == 0 && argv[i][0] != '0')
-			{
-				printf("Error\n");
-				return (1);
-			}
-			sum += num;
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		int num = atoi(argv[i]);
+
+		if (num == 0 && argv[i][0] != '0')
+		{
+			printf("Error\n");
+			return (1);
 		}
+		sum += num;
 	}
 	printf("%d\n", sum);
 

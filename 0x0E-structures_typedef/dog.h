d20c1429fad5f87a3889737c2435c6a67167ade5
HichamBC <yours.hichambc@gmail.com>
2023-07-17 18:45:04 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - define a struct dog type.
+ * @name: dog name.
+ * @age: dog age.
+ * @owner: dog owner.
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;

51004d6f2263e420fed666b9110a001926f04898
HichamBC <yours.hichambc@gmail.com>
2023-07-27 20:38:56 +0000

    0-print_list.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+size_t print_list(const list_t *h);
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -17,6 +19,5 @@ typedef struct list_s
 	struct list_s *next;
 } list_t;
 
-size_t print_list(const list_t *h);
 
 #endif

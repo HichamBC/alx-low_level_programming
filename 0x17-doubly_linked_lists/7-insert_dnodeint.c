cc62ed8736926e8a38cea6cce01531d34ab45abb
HichamBC <yours.hichambc@gmail.com>
2024-03-21 22:48:52 +0000

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -51,7 +51,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			return (add_dnodeint(h, n));
 		}
-		else if (p1->next == NULL && idx == node + 1)
+		else if (p1->next == NULL && idx >= node + 1)
 		{
 			return (add_dnodeint_end(h, n));
 		}
@@ -63,8 +63,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		p1 = p1->next;
 		node++;
 	}
-	if (idx == 0 && p1 == NULL)
-		return (add_dnodeint(h, n));
 
-	return (NULL);
+	return (add_dnodeint(h, n));
 }

ce0ac46b1ece3c832ce6deb26af3c6e8bd433055
HichamBC <yours.hichambc@gmail.com>
2024-03-21 23:46:03 +0000

    7-insert_dnodeint.c 4th modification

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			return (add_dnodeint_end(h, n));
 		}
+		else if (p1->next == NULL && idx > node +1)
+		{
+			return (NULL);
+		}
 		else if (node == idx && idx > 0)
 		{
 			temp = malloc(sizeof(dlistint_t));

6187d8455b539452650a8396919b0ccc900c469c
HichamBC <yours.hichambc@gmail.com>
2024-03-22 00:31:38 +0000

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -13,34 +13,43 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t  *ptr, *new_node;
-	ptr = *h;
+	dlistint_t *temp, *ptr = *h;
+	size_t node = 0;
+
+	while (ptr != NULL)
+	{
+		if (node == idx)
+			break;
+		ptr = ptr->next;
+		node++;
+	}
+	if (ptr == NULL && node < idx)
+		return (NULL);
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (idx != 1)
+	ptr = *h;
+	node = 0;
+	while (ptr != NULL && node < idx - 1)
 	{
 		ptr = ptr->next;
-		if (ptr == NULL)
-			return (NULL);
-
-		idx--;
+		node++;
 	}
-
-	if (ptr->next == NULL)
-		return (add_dnodeint_end(h, n));
-
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
+	if (ptr == NULL)
 		return (NULL);
 
-	new_node->prev = ptr;
-	new_node->next = ptr->next;
-	new_node->n = n;
-	ptr->next->prev = new_node;
-	ptr->next = new_node;
+	temp = malloc(sizeof(dlistint_t));
+	if (temp == NULL)
+		return (NULL);
 
-	return (new_node);
+	temp->n = n;
+	temp->prev = ptr;
+	temp->next = ptr->next;
+	if (ptr->next != NULL)
+		ptr->next->prev = temp;
+	ptr->next = temp;
+
+	return (temp);
 }
 
